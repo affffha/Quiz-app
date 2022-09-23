4153c1a84ff6f0ff0072c27026ff174bdb0d6e1b
IRAFRIX <naotomadoka@email.com>
2022-09-23 11:30:57 +0700

    quiz-app 23/03/2022

---
+++ src/component/Quiz.js
@@ -0,0 +1,12 @@
+import QuestionData from "../data/QuestionData";
+
+const Quiz= () =>{
+    console.log(QuestionData);
+    return(
+        <div className="quiz">
+            <h1>Quiz Component</h1>
+        </div>
+    )
+}
+
+export default Quiz;
